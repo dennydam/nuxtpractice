34f166edd04593873f8cde2549719b75b34dff29
dennydam <a0989159555@gmail.com>
2024-05-10 14:57:35 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import { defineNuxtConfig } from 'nuxt'
 const path = require('path')
+console.log('test')
 export default defineNuxtConfig({
   devtools: { enabled: true },
   // nitro: {
